45f4b108e1995f848680ed6c384c2ab639541eb7
RootenberG <savichevdenis244@gmail.com>
2020-06-25 14:12:06 +0300

    rm permissions

--- news/settings.py
+++ news/settings.py
@@ -126,7 +126,6 @@ USE_TZ = True
 
 # rest
 REST_FRAMEWORK = {
-    "DEFAULT_PERMISSION_CLASSES": ["rest_framework.permissions.IsAuthenticated",],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
